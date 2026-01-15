d4d5ccec8813b4923441a6147581478b6e27af93
Sneha-6392 <sneha.mauryasm02@gmail.com>
2026-01-15 16:33:41 +0530

    js

--- index.js
+++
@@ -1,8 +0,0 @@
-// const math = require('./math');
-// console.log(math.add(2, 3));
-// console.log(math.subtract(5, 2));
-
-const logActivity = require("./logger");
-logActivity("User logged in");
-logActivity("User viewed dashboard");
-logActivity("User logged out");
